b3f42dfa9a558592ec680c646d5a3e63e3c249e0
Predrag Stefanovic <predrag.stefanovic98@gmail.com>
2021-11-06 13:20:01 +0100

    Added test case

---
+++ src/test/java/com/example/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class ApplicationTest {
+
+    @Test
+    public void simpleTest() {
+        assertEquals(3, 3);
+    }
+}
